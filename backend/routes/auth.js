b4460a8b0160faa5a3994be12c367832f60d1d22
Amol Dogra <amoldogra01@gmail.com>
2022-02-08 15:53:29 +0530

    Hashing, Salt and Webtoken added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'devismakinginotebook';
 
 const router = express.Router();
 // create a user using : Post  "/api/auth/createuser"
@@ -13,6 +16,9 @@ async (req, res)=>{
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+
+
     // check weather user exist already
     try{
 
@@ -21,15 +27,28 @@ async (req, res)=>{
     if(user){
       return res.status(400).json({error: "User Already Exists"})
     }
+
+    // creating password into hash and adding salt init  using npm package "bcrypt"
+    
+    const salt = await bcrypt.genSalt(10);  
+     const secPass =  await bcrypt.hash(req.body.password, salt); 
    user = await  User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email
-      })
-   //      .then(user => res.json(user));
- 
-      res.json({user})
-    }catch(error){
+      });
+
+      // creating a unique token for every user to login in it using npm package "jsonwebtoken"
+      
+        const data = {
+          user:{
+            id : user.id
+          }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken})
+    }
+    catch(error){
     console.error(error.message);
     res.status(500).send("Some Error Occured");
     }

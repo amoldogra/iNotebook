9cd2c315311f1414ec8f1fba0543df0d00261026
Amol Dogra <amoldogra01@gmail.com>
2022-02-08 17:19:17 +0530

    Authentication Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,91 +1,111 @@
-const express = require('express');
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const { body, validationResult } = require('express-validator');
-var jwt = require('jsonwebtoken');
-const JWT_SECRET = 'devismakinginotebook';
-
+const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const { body, validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "devismakinginotebook";
+var fetchuser = require("../middleware/fetchuser");
 const router = express.Router();
-// create a user using : Post  "/api/auth/createuser"
-router.post('/createuser', [body('email', 'Enter a valid email').isEmail(),
-                  body('name', 'Enter a valid name').isLength({ min: 3 }),
-                  body('password', 'Enter correct password').isLength({ min: 5 })],
-                  // if there is error return bad request
-async (req, res)=>{ 
+
+//  Route 1 ------>   create a user using : Post  "/api/auth/createuser"
+
+router.post(
+  "/createuser",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("password", "Enter correct password").isLength({ min: 5 }),
+  ],
+  // if there is error return bad request
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-
-
     // check weather user exist already
-    try{
-
-    
-    let user = await User.findOne({email: req.body.email});  
-    if(user){
-      return res.status(400).json({error: "User Already Exists"})
-    }
-
-    // creating password into hash and adding salt init  using npm package "bcrypt"
-    
-    const salt = await bcrypt.genSalt(10);  
-     const secPass =  await bcrypt.hash(req.body.password, salt); 
-   user = await  User.create({
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "User Already Exists" });
+      }
+      // creating password into hash and adding salt init  using npm package "bcrypt"
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
         name: req.body.name,
         password: secPass,
-        email: req.body.email
+        email: req.body.email,
       });
-
       // creating a unique token for every user to login in it using npm package "jsonwebtoken"
 
-        const data = {
-          user:{
-            id : user.id
-          }
-        }
-        const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({authtoken})
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
-    catch(error){
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-    }
-})
-
-// authenticate a user using : POST localhost:5000/api/auth/login
-
-router.post('/login', [body('email', 'Enter a valid email').isEmail(),
-                  body('password', 'Password cannot be blanked').exists()
-                ],
-                 
-async (req, res)=> { 
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
   }
-  const {email, password} = req.body;
-  try {
-    let user =  await User.findOne({email});
-    if(!user){
-      return res.status(400).json({error: "Try to login with corect credentials"});
-    }
-    const passCompare = await  bcrypt.compare(password, user.password );
-    if(!passCompare){
-      return res.status(400).json({error: "Try to login with corect credentials"});
-    }
+);
 
-    const data = {
-      user:{
-        id : user.id
+// Route 2 ------->  authenticate a user using : POST localhost:5000/api/auth/login
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blanked").exists(),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Try to login with corect credentials" });
       }
+      const passCompare = await bcrypt.compare(password, user.password);
+      if (!passCompare) {
+        return res
+          .status(400)
+          .json({ error: "Try to login with corect credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
-    const authtoken = jwt.sign(data, JWT_SECRET);
-    res.json({authtoken})
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
   }
-})
-module.exports = router
+);
+
+// Route 3 ------->  get logged in  user using : POST localhost:5000/api/auth/getuser
+
+router.post("/getuser", fetchuser,  async (req, res) => {
+    try {
+      userId = req.user.id;
+      const user = await User.findById(userId).select("-password");
+      res.send(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+module.exports = router;

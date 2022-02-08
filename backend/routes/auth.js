639741d708acd5afbe9a79ef188b9387cf5b090e
Amol Dogra <amoldogra01@gmail.com>
2022-02-08 16:17:37 +0530

    login added in DB

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -39,7 +39,7 @@ async (req, res)=>{
       });
 
       // creating a unique token for every user to login in it using npm package "jsonwebtoken"
-      
+
         const data = {
           user:{
             id : user.id
@@ -50,8 +50,42 @@ async (req, res)=>{
     }
     catch(error){
     console.error(error.message);
-    res.status(500).send("Some Error Occured");
+    res.status(500).send("Internal Server Error");
     }
 })
 
+// authenticate a user using : POST localhost:5000/api/auth/login
+
+router.post('/login', [body('email', 'Enter a valid email').isEmail(),
+                  body('password', 'Password cannot be blanked').exists()
+                ],
+                 
+async (req, res)=> { 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {email, password} = req.body;
+  try {
+    let user =  await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error: "Try to login with corect credentials"});
+    }
+    const passCompare = await  bcrypt.compare(password, user.password );
+    if(!passCompare){
+      return res.status(400).json({error: "Try to login with corect credentials"});
+    }
+
+    const data = {
+      user:{
+        id : user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({authtoken})
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 module.exports = router
